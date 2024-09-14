181c8ae1e67e8785384507b753d7bdb706f9475a
jivy26 <jivy26@gmail.com>
2024-09-14 09:18:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,8 @@ from setuptools import setup
 
 setup(
     name='nolimit-scanner',
-    version='1.1',
+    version='1.2',
     py_modules=['nolimit'],
-    install_requires=[
-        'aiofiles==0.8.0',
-        'colorama==0.4.4',
-        'rich==10.16.2',
-        'scapy==2.4.5',
-        'tqdm==4.62.3'
-    ],
     author='jivy26',
     author_email='jivy26@gmail.com',
     description='Inspired by Masscan, NoLimit is a python based asynchronous port scanner and service enumeration.',
@@ -21,4 +14,9 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'nolimit = nolimit:main',
+        ],
+    },
 )
